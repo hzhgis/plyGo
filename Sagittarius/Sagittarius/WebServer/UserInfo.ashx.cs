4b910374611ade2e07d8e0bc2e8209dc975195dd
zlbandy <zlbandy@163.com>
2016-12-07 22:28:01 +0800

    无聊而已

---
+++ Sagittarius/Sagittarius/WebServer/UserInfo.ashx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sagittarius.WebServer
+{
+    /// <summary>
+    /// UserInfo 的摘要说明
+    /// </summary>
+    public class UserInfo : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string userid = context.Request.Params["id"].ToString();
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(userid);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Sagittarius/Sagittarius/default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="Sagittarius.defult" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <p>hello world!</p>
+        <asp:Button runat="server" OnClick="Unnamed_Click" Text="hello"/>
+    </div>
+    </form>
+</body>
+</html>
